992caa3ac5e87ec6df6b3544be93a0859787492f
Mykhailo Stryzhka <misha.strizhcko@gmail.com>
2023-07-02 03:52:10 +0200

    Adds MongoDB

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -1,61 +1,78 @@
-const contacts = require("../models/contacts");
 const { HttpError, ctrlWrapper } = require("../helpers");
-const { addSchema } = require("../schemas");
+const Contact = require("../models/contact");
+const { addSchema, updateFavoriteSchema } = require("../schemas");
 
 const getContacts = async (req, res) => {
-    const result = await contacts.listContacts();
-    res.status(200).json(result);
+  const result = await Contact.find();
+  res.status(200).json(result);
 };
 const getContactById = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.getContactById(id);
+  const { id } = req.params;
+  const result = await Contact.findById(id);
+  console.log(result);
 
-    if (!result) {
-      throw HttpError(404, "Not fount");
-    }
+  if (!result) {
+    throw HttpError(404, "Not fount");
+  }
 
-    res.status(200).json(result);
+  res.status(200).json(result);
 };
 const addContact = async (req, res) => {
-    const data = req.body;
-    const { error } = addSchema.validate(data);
+  const data = req.body;
+  const { error } = addSchema.validate(data);
 
-    if (error) {
-      throw HttpError(400, error.message);
-    }
+  if (error) {
+    throw HttpError(400, error.message);
+  }
 
-    const result = await contacts.addContact(data);
-    res.status(201).json(result);
+  const result = await Contact.create(data);
+  res.status(201).json(result);
 };
 const removeContact = async (req, res) => {
-    const { id } = req.params;
-    const result = await contacts.removeContact(id);
+  const { id } = req.params;
+  const result = await Contact.findByIdAndRemove(id);
 
-    if (!result) {
-      throw HttpError(404, "Not fount");
-    }
+  if (!result) {
+    throw HttpError(404, "Not fount");
+  }
 
-    res.status(200).json({ message: "contact deleted" });
+  res.status(200).json({ message: "contact deleted" });
 };
 const updateContact = async (req, res) => {
-    const { id } = req.params;
-    const data = req.body;
+  const { id } = req.params;
+  const data = req.body;
 
-    if (!data.name && !data.email && !data.phone) {
-      throw HttpError(404, "missing fields");
-    }
+  if (!data.name && !data.email && !data.phone) {
+    throw HttpError(404, "missing fields");
+  }
 
-    const { error } = addSchema.validate(data);
-    if (error) {
-      throw HttpError(400, error.message);
-    }
+  const { error } = addSchema.validate(data);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
 
-    const result = await contacts.updateContact(id, data);
-    if (!result) {
-      throw HttpError(404, "Not fount");
-    }
+  const result = await Contact.findByIdAndUpdate(id, data, {new: true});
+  if (!result) {
+    throw HttpError(404, "Not fount");
+  }
 
-    res.status(200).json(result);
+  res.status(200).json(result);
+};
+const updateStatusContact = async (req, res) => {
+  const { id } = req.params;
+  const data = req.body;
+
+  const { error } = updateFavoriteSchema.validate(data);
+  if (error) {
+    throw HttpError(400, error.message);
+  }
+
+  const result = await Contact.findByIdAndUpdate(id, data, {new: true});
+  if (!result) {
+    throw HttpError(404, "Not fount");
+  }
+
+  res.status(200).json(result);
 };
 
 module.exports = {
@@ -64,4 +81,5 @@ module.exports = {
   addContact: ctrlWrapper(addContact),
   removeContact: ctrlWrapper(removeContact),
   updateContact: ctrlWrapper(updateContact),
+  updateStatusContact: ctrlWrapper(updateStatusContact),
 };
